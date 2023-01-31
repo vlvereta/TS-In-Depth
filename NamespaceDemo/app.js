0ce7cbb8d983218794a51d662bc121ddbba047d3
Vladyslav Vereta <vlvereta.92@gmail.com>
2023-01-31 21:11:49 +0200

    Adds namespaced subproject.

---
+++ NamespaceDemo/app.js
@@ -0,0 +1,6 @@
+/// <reference path='./utility-functions.ts' />
+var result = Utility.maxBooksAllowed(15);
+console.log(result);
+var util = Utility.Fees;
+var result2 = util.calculateLateFee(30);
+console.log(result2);
---
+++ NamespaceDemo/app.ts
@@ -0,0 +1,8 @@
+/// <reference path='utility-functions.ts' />
+
+const result = Utility.maxBooksAllowed(15);
+console.log(result);
+
+import util = Utility.Fees;
+const result2 = util.calculateLateFee(30);
+console.log(result2);
